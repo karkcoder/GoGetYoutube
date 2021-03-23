2b7a86453825b102c9fe9d128019f84d6337227e
Tim Karki <urkarki@gmail.com>
2021-03-22 22:28:26 -0600

    removing shell script

--- GoGetYoutube/installyoutubedl.sh
+++
@@ -1,4 +0,0 @@
-﻿sudo apt install python3
-sudo ln -s /usr/bin/python3 /usr/bin/python
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
